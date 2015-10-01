d8eede88dee0f2ac65d7b16063564307df32dbce
Anindya Bhattacharyya <bat020@gmail.com>
2015-10-01 12:25:18 +0100

    it's in colourgitad

--- src/app.js
+++ src/app.js
@@ -1,26 +1,34 @@
 $( document ).ready(function() {
   var thermostat = new Thermostat();
+  updateTemp();
 
-  $('#show_temperature').text(thermostat.temperature);
+  function updateTemp() {
+    var temp = thermostat.temperature
+    var colour = '#CC1559';
+    if (temp < 25) {colour = '#FF6600'};
+    if (temp < 18) {colour = '#2C6700'};
+    $('#temp_display').attr('style', 'color:' + colour);
+    $('#show_temperature').text(temp);
+  };
 
   $('#up').click(function() {
     thermostat.temperatureRaise();
-    $('#show_temperature').text(thermostat.temperature);
+    updateTemp();
   });
 
   $('#down').click(function() {
     thermostat.temperatureLower();
-    $('#show_temperature').text(thermostat.temperature);
+    updateTemp();
   });
 
   $('#reset').click(function() {
     thermostat.reset();
-    $('#show_temperature').text(thermostat.temperature);
+    updateTemp();
   });
 
   $('#power_save').click(function() {
     thermostat.togglePowerSaving();
-    $('#show_temperature').text(thermostat.temperature);
+    updateTemp();
   });
 
 });
